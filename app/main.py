c9cf32b5def7b072d324345bbb0ee811483e4445
jacobalbert3 <jralbert@umich.edu>
2025-05-22 14:39:01 -0400

    cors issues

--- app/main.py
+++ app/main.py
@@ -44,7 +44,6 @@ app.add_middleware(SSLMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"]
